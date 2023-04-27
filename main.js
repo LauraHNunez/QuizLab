3ff482f3e9ffcced5caa5a909febd711d5dd91fb
LauraHNunez <110181542+LauraHNunez@users.noreply.github.com>
2023-04-27 17:10:54 +0200

    Update main.js

--- main.js
+++ main.js
@@ -279,7 +279,7 @@ function comprovarResposta(){
    console.log("NumeroPregunta"+respostes[resposta][0])
   if (respostes[resposta] == (""+imageID)){
        document.getElementById("pregunta").innerHTML = "Correcto " + numeroimagen;  
-       ferPregunta()
+       setTimeout(ferPregunta, 8000)
        document.getElementById("pregunta").innerHTML = preguntas[ferPregunta()][2]
   }else{
       document.getElementById("pregunta").innerHTML = "Incorrecto " + numeroimagen;    
